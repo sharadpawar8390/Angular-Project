59e0f9be57b7cc47b8dac0e56abb6ccc54908441
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-05 00:50:21 +0530

    Add files via upload

---
+++ src_Component1/app/infosystem/infosystem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infosystem',
+  standalone: true,
+  imports: [],
+  templateUrl: './infosystem.component.html',
+  styleUrl: './infosystem.component.css'
+})
+export class InfosystemComponent {
+
+}
