4dde4a146e9963a5b39d842496c4cf396b6e0452
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-05 13:36:22 +0530

    Add files via upload

---
+++ src_Modular/app/marvellous/second/second.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  template: `
+    <h1>Inside Second component </h1>
+  `,
+  styles:[] 
+})
+export class SecondComponent {
+
+}
