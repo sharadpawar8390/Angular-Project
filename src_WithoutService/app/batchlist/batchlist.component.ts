3ed122875e208023f07baf6f471d76e01069ef5f
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-05 17:00:41 +0530

    Add files via upload

---
+++ src_WithoutService/app/batchlist/batchlist.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-batchlist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './batchlist.component.html',
+  styleUrl: './batchlist.component.css'
+})
+export class BatchlistComponent 
+{
+  public batches = [
+    {"Name" : "Pre Placement Activity","Duration" : "3 Months","Fees" : 19500},
+    {"Name" : "Logic Building","Duration" : "3.5 Months","Fees" : 20500},
+    {"Name" : "Python Machine Learning","Duration" : "4 Months","Fees" : 21000},
+    {"Name" : "Angular with Mean","Duration" : "4.5 Months","Fees" : 21500},
+  ];
+}
