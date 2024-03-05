d0b42d4e3e87bad3abd6a867fddfa1d2fd469817
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-05 22:10:59 +0530

    Add files via upload

---
+++ MEAN/client/src_client/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { MarvellousService } from './marvellous.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration(),
+    MarvellousService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
