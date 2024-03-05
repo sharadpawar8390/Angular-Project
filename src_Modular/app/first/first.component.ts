d93d4583f7697d63c4a3efae89b07fb474227659
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-05 13:31:16 +0530

    Add files via upload

---
+++ src_Modular/app/first/first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  template: `
+  <h1>Inside first component</h1>
+  `,
+  styles: []
+})
+export class FirstComponent {
+
+}
