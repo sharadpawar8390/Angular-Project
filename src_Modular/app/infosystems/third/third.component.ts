0db221061925793788d0c29e63e2b3a94a59031b
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-05 13:34:14 +0530

    Add files via upload

---
+++ src_Modular/app/infosystems/third/third.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third',
+  template: `
+  <h1>Inside third component</h1>
+  `,
+  styles:[]
+})
+export class ThirdComponent {
+
+}
