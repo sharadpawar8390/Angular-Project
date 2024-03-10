5f7844a9b4215bca034bc1e0163a90716b55c481
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-10 12:46:17 +0530

    Add files via upload

---
+++ EVENT HUB/src_Event Hub/app/helpdesk/helpdesk.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-helpdesk',
+  standalone: true,
+  imports: [],
+  templateUrl: './helpdesk.component.html'
+})
+export class HelpdeskComponent {
+
+}
