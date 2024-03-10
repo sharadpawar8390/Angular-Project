c470b1e35c1be483eb59bb77ae3bc87ad6e7324c
sharadpawar8390 <155647847+sharadpawar8390@users.noreply.github.com>
2024-03-10 12:49:35 +0530

    Add files via upload

---
+++ EVENT HUB/src_Event Hub/app/takeadmission/takeadmision.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+ 
+
+@Component({
+  selector: 'app-takeadmision',
+  standalone: true,
+  imports: [],
+  templateUrl: './takeadmision.component.html',
+ 
+})
+export class TakeadmisionComponent 
+{
+   
+}
